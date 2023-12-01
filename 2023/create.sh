0b8482c6ab03328708d613789fd52107c29847f3
Dirivial <alexander.kadeby@gmail.com>
2023-12-01 03:54:53 +0100

    Updated script to this years problems

--- 2023/create.sh
+++ 2023/create.sh
@@ -2,13 +2,13 @@
 
 day=${1##+(0)}
 if [ -z "${day}" ]; then
-  echo "usage: ./$0 day"
-  exit
+	echo "usage: ./$0 day"
+	exit
 fi
 
 if [ -z "${AOC_SESSION}" ]; then
-  echo "You need to set AOC_SESSION. Aborting..."
-  exit
+	echo "You need to set AOC_SESSION. Aborting..."
+	exit
 fi
 
 echo "Creating folder '$day'"
@@ -20,10 +20,10 @@ touch a.ts
 touch b.ts
 touch test.txt
 
-echo "const _input = Deno.readFileSync('input.txt');" > a.ts
-echo "const _input = Deno.readFileSync('input.txt');" > b.ts
+echo "const _input = Deno.readFileSync('input.txt');" >a.ts
+echo "const _input = Deno.readFileSync('input.txt');" >b.ts
 
 echo "Fetching input data"
-curl -s "https://adventofcode.com/2022/day/$day/input" --cookie "session=${AOC_SESSION}" -o input.txt
+curl -s "https://adventofcode.com/2023/day/$day/input" --cookie "session=${AOC_SESSION}" -o input.txt
 
 echo "Done."
