9bcff4aa8e4123c986b265e1402ddda7276fcf1e
Dirivial <alexander.kadeby@gmail.com>
2023-12-01 04:27:36 +0100

    Doublechecked that the test actually worked

--- 2023/test.ts
+++ 2023/test.ts
@@ -1,1 +1,7 @@
-const _input = await Deno.readTextFile("./input.txt");
+const _input = await Deno.readTextFile("./testInput.txt");
+
+const input = _input.split("\n");
+
+input.forEach((line) => {
+  console.log(line);
+});
