bb958c26d61366b0ab626db97d0c00f1708a6300
Dirivial <alexander.kadeby@gmail.com>
2023-12-01 04:35:43 +0100

    Looked up how to switch lsp in neovim, added comment

--- 2023/test.ts
+++ 2023/test.ts
@@ -1,7 +1,12 @@
+// LspStop -> LspStart Denols
+// To get back, run LspStop -> LspStart tsserver
+
 const _input = await Deno.readTextFile("./testInput.txt");
 
 const input = _input.split("\n");
 
-input.forEach((line) => {
+input.forEach((line: string) => {
   console.log(line);
 });
+
+export {};
