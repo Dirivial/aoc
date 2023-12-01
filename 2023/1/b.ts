8891d05c742393b39ee6324137d72b66d803e5e7
Dirivial <alexander.kadeby@gmail.com>
2023-12-01 18:28:59 +0100

    Less convoluted version

--- 2023/1/b.ts
+++ 2023/1/b.ts
@@ -26,28 +26,6 @@ const validStuff = [
   "9",
 ];
 
-const validStuffRev = [
-  "eno",
-  "owt",
-  "eerht",
-  "ruof",
-  "evif",
-  "xis",
-  "neves",
-  "thgie",
-  "enin",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-];
-
 const translation = new Map();
 translation.set("one", "1");
 translation.set("two", "2");
@@ -59,46 +37,32 @@ translation.set("seven", "7");
 translation.set("eight", "8");
 translation.set("nine", "9");
 translation.set("zero", "0");
-translation.set("eno", "1");
-translation.set("owt", "2");
-translation.set("eerht", "3");
-translation.set("ruof", "4");
-translation.set("evif", "5");
-translation.set("xis", "6");
-translation.set("neves", "7");
-translation.set("thgie", "8");
-translation.set("enin", "9");
-translation.set("orez", "0");
 
 input.forEach((line) => {
-  let i = 0;
+  let l = 0;
+  let r = line.length - 1;
   let a = "";
   let b = "";
 
   while (a === "") {
-    const substr = line.substring(i, i + 5);
+    const substr = line.substring(l);
     for (const valid of validStuff) {
-      if (substr.startsWith(valid)) {
+      if (a === "" && substr.startsWith(valid)) {
         a = valid;
         break;
       }
     }
-    i++;
+    l++;
   }
-
-  const revLine = line.split("").reverse().join("");
-  i = 0;
-
-  while (b === "" && i < line.length) {
-    const substr = revLine.substring(i, i + 5);
-    for (const valid of validStuffRev) {
+  while (b === "") {
+    const substr = line.substring(r);
+    for (const valid of validStuff) {
       if (substr.startsWith(valid)) {
         b = valid;
-        console.log(b);
         break;
       }
     }
-    i++;
+    r--;
   }
 
   if (translation.has(a)) {
