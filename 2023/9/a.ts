e7cfd6e0c4b82a1f15995e7c4dc4b0e72d3a85a3
Dirivial <alexander.kadeby@gmail.com>
2023-12-09 09:11:01 +0100

    Recursive way looks a lot neater

--- 2023/9/a.ts
+++ 2023/9/a.ts
@@ -2,42 +2,21 @@ const _input = await Deno.readTextFile("input.txt");
 
 const input = _input.split("\n").map((v) => v.split(" ").map((n) => Number(n)));
 
-const isAllZeros = (arr: number[]) => {
-  return arr.every((v) => v === 0);
-};
-
-const computeLines = (initialLine: number[]): number[][] => {
-  const lines: number[][] = [[...initialLine]];
-  let newLine: number[] = [...initialLine];
-  let l = 0;
-
-  while (!isAllZeros(newLine)) {
-    newLine = [];
-    for (let i = 1; i < lines[l].length; i++) {
-      const diff = lines[l][i] - lines[l][i - 1];
-      newLine.push(diff);
-    }
-    l++;
-    lines.push([...newLine]);
+const recursiveAnswer = (line: number[]): number => {
+  if (line.every((v) => v === 0)) {
+    return 0;
   }
-  return lines;
-};
 
-const computeNextValue = (lines: number[][]): number => {
-  for (let i = lines.length - 1; i > 0; i--) {
-    const a = lines[i].at(-1);
-    const b = lines[i - 1].at(-1);
-    lines[i - 1].push((a ?? 0) + (b ?? 0));
+  const newLine: number[] = [];
+  for (let i = 1; i < line.length; i++) {
+    newLine.push(line[i] - line[i - 1]);
   }
-  return lines[0].at(-1) ?? 0;
+  return (line.at(-1) ?? 0) + recursiveAnswer(newLine);
 };
 
 let sum = 0;
-
 input.forEach((line) => {
-  const lines = computeLines(line);
-  const next = computeNextValue(lines);
-  sum += next;
+  sum += recursiveAnswer(line);
 });
 
 console.log(sum);
